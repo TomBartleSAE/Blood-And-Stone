3b6ad733995b657925962b62cd0bee1f08c5a84b
AaronMcDougall <1021572@student.sae.edu.au>
2022-02-27 22:13:17 +1100

    Few Changes
    Apple sprites added for testing resolution; bezier added for day/night; TMPro added for text related activites

--- Assets/Aaron/Day Night Cycle/DayNightManager.cs
+++ Assets/Aaron/Day Night Cycle/DayNightManager.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 
 public class DayNightManager : MonoBehaviour
 {
+    public enum DayPhase
+    {
+        Night,
+        Day
+    }
+
+    public DayPhase dayPhase;
 
     public float timeSpeedMultiplier;
     public float time;
 
-    public Transform sunRise;
-    public Transform sunSet;
+    public Transform p1Sunrise;
+    public Transform p4Sunset;
+    public Transform p2;
+    public Transform p3;
+
+    public Vector2 point;
 
     public GameObject sun;
+    public GameObject moon;
 
     public float timeElapsed;
     public float lerpDuration;
@@ -24,19 +36,65 @@ public class DayNightManager : MonoBehaviour
     {
         StartCoroutine(TimeCycle());
         lerpDuration = (12 * timeSpeedMultiplier);
+        time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (timeElapsed < lerpDuration)
+        // if (timeElapsed < lerpDuration)
+        // {
+        //     sun.transform.position = Vector2.Lerp(p1Sunrise.position, p4Sunset.position, (timeElapsed/lerpDuration));
+        //     timeElapsed += Time.deltaTime;
+        // }
+
+        time += Time.deltaTime;
+
+        if (time > timeSpeedMultiplier)
         {
-            sun.transform.position = Vector3.Lerp(sunRise.position, sunSet.position, (timeElapsed/lerpDuration));
-            timeElapsed += Time.deltaTime;
+            time = 0;
+            timeElapsed += 1;
+        }
+
+        Vector2 lerpA = Vector2.Lerp(p1Sunrise.position, p2.position, (timeElapsed/lerpDuration));
+        Vector2 lerpB = Vector2.Lerp(p2.position, p3.position, (timeElapsed/lerpDuration));
+        Vector2 lerpC = Vector2.Lerp(p3.position, p4Sunset.position, (timeElapsed/lerpDuration));
+        Vector2 lerpD = Vector2.Lerp(lerpA, lerpB, (timeElapsed/lerpDuration));
+        Vector2 lerpE = Vector2.Lerp(lerpB, lerpC, (timeElapsed/lerpDuration));
+
+        point = Vector2.Lerp(lerpD, lerpE, (timeElapsed/lerpDuration));
+        
+        if (dayPhase == DayPhase.Day)
+        {
+            sun.transform.position = point;
+        }
+        
+        else if (dayPhase == DayPhase.Night)
+        {
+            moon.transform.position = point;
+        }
+
+        //Probably extremely long winded way to do this tbh
+        if (timeElapsed >= lerpDuration)
+        {
+
+            timeElapsed = 0;
+            
+            if (dayPhase == DayPhase.Day)
+            {
+                dayPhase = DayPhase.Night;
+            }
+
+            else if(dayPhase == DayPhase.Night)
+            {
+                dayPhase = DayPhase.Day;
+            }
         }
     }
 
+    //Doesn't seem to want to function properly
+    //Was used to run time smoothly
     public IEnumerator TimeCycle()
     {
         for (int i = 1; i < 13; i++)
