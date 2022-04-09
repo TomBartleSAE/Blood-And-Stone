be1735ae721c06b3f59054a588fbeb4dc60f0a98
AaronMcDougall <72240339+AaronMcDougall@users.noreply.github.com>
2022-04-09 20:10:30 +1000

    guard start

--- Assets/Aaron/AI Behaviour/Guards/GuardSense.cs
+++ Assets/Aaron/AI Behaviour/Guards/GuardSense.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using Anthill.AI;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GuardSense : MonoBehaviour, ISense
 {
     public enum GuardBools
     {
-        
+        hasTarget = 0,
+        isAlert = 1,
+        inRange = 2,
+        targetCaptured = 3
     }
 
     public void CollectConditions(AntAIAgent aAgent, AntAICondition aWorldState)
     {
         aWorldState.BeginUpdate(aAgent.planner);
-        
-        
+
+        aWorldState.Set(GuardBools.hasTarget, aAgent.GetComponent<GuardModel>().hasTarget);
+        aWorldState.Set(GuardBools.isAlert, aAgent.GetComponent<GuardModel>().isAlert);
+        aWorldState.Set(GuardBools.inRange, aAgent.GetComponent<GuardModel>().inRange);
+        aWorldState.Set(GuardBools.targetCaptured, aAgent.GetComponent<GuardModel>().targetCaptured);
         
         aWorldState.EndUpdate();
     }
