f73f6c1823d3dc125e2af0e7cc535146442e7adc
AaronMcDougall <72240339+AaronMcDougall@users.noreply.github.com>
2022-04-25 16:06:56 +1000

    NPCManager ManagerBase now

--- Assets/Prototype/Scripts/NPCManager.cs
+++ Assets/Prototype/Scripts/NPCManager.cs
@@ -4,15 +4,10 @@ using System.Collections.Generic;
 using Tanks;
 using UnityEngine;
 
-public class NPCManager : MonoBehaviour
+public class NPCManager : ManagerBase<NPCManager>
 {
     public List<GameObject> Villagers = new List<GameObject>();
     public List<GameObject> Ghouls = new List<GameObject>();
     public List<GameObject> Soldiers = new List<GameObject>();
     public List<GameObject> Guards = new List<GameObject>();
-
-    public void Start()
-    {
-
-    }
 }
