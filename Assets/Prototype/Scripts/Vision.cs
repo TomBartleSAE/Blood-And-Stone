93d9390bf116561b94895dae791af7a4026d3a25
Tom Bartle <tombartle96@gmail.com>
2022-06-14 14:44:50 +1000

    Added debug lines to vision

--- Assets/Prototype/Scripts/Vision.cs
+++ Assets/Prototype/Scripts/Vision.cs
@@ -23,4 +23,18 @@ public class Vision : MonoBehaviour
 
         return false;
     }
+
+    public void OnDrawGizmosSelected()
+    {
+        Quaternion leftRayRotation = Quaternion.AngleAxis(-angle, Vector3.up);
+        Quaternion rightRayRotation = Quaternion.AngleAxis(angle, Vector3.up);
+
+        Vector3 leftRayDirection = leftRayRotation * transform.forward;
+        Vector3 rightRayDirection = rightRayRotation * transform.forward;
+        
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, leftRayDirection * distance);
+        Gizmos.DrawRay(transform.position, rightRayDirection * distance);
+        Gizmos.DrawLine(transform.position + leftRayDirection * distance, transform.position + rightRayDirection * distance);
+    }
 }
