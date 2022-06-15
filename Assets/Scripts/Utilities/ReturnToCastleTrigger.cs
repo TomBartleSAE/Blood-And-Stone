10deb5fa8b2c9536c5753f473765bf73f106672b
AaronMcDougall <1021572@student.sae.edu.au>
2022-06-15 11:45:08 +1000

    Added response for guard/ghoul entering trigger

--- Assets/Scripts/Utilities/ReturnToCastleTrigger.cs
+++ Assets/Scripts/Utilities/ReturnToCastleTrigger.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ReturnToCastleTrigger : MonoBehaviour
@@ -10,6 +11,12 @@ public class ReturnToCastleTrigger : MonoBehaviour
         if (other.GetComponent<VampireModel>())
         {
             GameManager.Instance.CallPhaseChange("DayTest", "NightTest", GameManager.Instance.dayPhaseState);
+        }
+
+        //added to remove converted guard from view - AM
+        if (other.GetComponent<GuardModel>() && other.GetComponent<GuardModel>().isDead)
+        {
+            other.gameObject.SetActive(false);
         }
     }
 }
