27cb8d2bc9ce61a9d7b9d50cd0d63d196214772f
Tom Bartle <tombartle96@gmail.com>
2022-06-17 11:31:18 +1000

    Actually added the Player Manager this time

---
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : ManagerBase<PlayerManager>
+{
+    public int currentBlood;
+    public int maxBlood = 50;
+    
+    /// <summary>
+    /// Sends out how much the blood has changed by
+    /// </summary>
+    public event Action<int> BloodChangedEvent;
+
+    /// <summary>
+    /// Sends out how much the max blood has changed by
+    /// </summary>
+    public event Action<int> MaxBloodChangedEvent;
+
+    public void ChangeBlood(int amount)
+    {
+        if (currentBlood + amount > maxBlood)
+        {
+            amount = maxBlood - currentBlood;
+        }
+        
+        currentBlood += amount;
+        
+        BloodChangedEvent?.Invoke(amount);
+    }
+
+    public void ChangeMaxBlood(int amount)
+    {
+        maxBlood += amount;
+
+        Mathf.Clamp(currentBlood, 0, maxBlood);
+        
+        MaxBloodChangedEvent?.Invoke(amount);
+    }
+}
