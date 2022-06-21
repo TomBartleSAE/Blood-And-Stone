d2a37401d0f08eff4f9a97cdb2faf92e077d91de
Tom Bartle <tombartle96@gmail.com>
2022-06-21 13:39:02 +1000

    Small changes before build

--- Assets/Prefabs/AI Behaviour/Guards/States/InvestigatingState.cs
+++ Assets/Prefabs/AI Behaviour/Guards/States/InvestigatingState.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Anthill.AI;
-using Unity.VisualScripting;
-using UnityEditor.Profiling.Memory.Experimental;
 using UnityEngine;
 
 public class InvestigatingState : AntAIState
