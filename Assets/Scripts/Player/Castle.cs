2b88e8b73bba163244856561d64c086b4defb690
Tom Bartle <tombartle96@gmail.com>
2022-07-02 13:04:40 +1000

    Added tooltips for objects

--- Assets/Scripts/Player/Castle.cs
+++ Assets/Scripts/Player/Castle.cs
@@ -3,16 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using Tom;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class Castle : MonoBehaviour
 {
-    public Camera cam;
-    public LayerMask castleLayer;
-    
     public Health health;
     public int level = 1;
     
@@ -23,39 +17,14 @@ public class Castle : MonoBehaviour
     public int[] ghoulPopcaps = new int[4];
     public GameObject[] meshes = new GameObject[4];
 
-    public GameObject tooltip;
-    public GraphicRaycaster graphicRaycaster;
-
     private void OnEnable()
     {
         health.DeathEvent += DestroyCastle;
-        InputManager.Instance.OnLeftClickEvent += PerformLeftClick;
-        ShowTooltip(false);
     }
 
     private void OnDisable()
     {
         health.DeathEvent -= DestroyCastle;
-        InputManager.Instance.OnLeftClickEvent -= PerformLeftClick;
-    }
-
-    private void PerformLeftClick(ClickEventArgs args)
-    {
-        Ray ray = cam.ScreenPointToRay(args.mousePosition);
-
-        PointerEventData data = new PointerEventData(EventSystem.current);
-        data.position = args.mousePosition;
-        List<RaycastResult> results = new List<RaycastResult>();
-        graphicRaycaster.Raycast(data, results);
-        
-        if (Physics.Raycast(ray, Mathf.Infinity, castleLayer))
-        {
-            ShowTooltip(true);
-        }
-        else if (results.Count == 0)
-        {
-            ShowTooltip(false);
-        }
     }
 
     public void DestroyCastle(GameObject castle)
@@ -63,16 +32,12 @@ public class Castle : MonoBehaviour
         StartCoroutine(ReturnToMenu());
     }
 
+    // TODO: Move this to the GameManager
     public IEnumerator ReturnToMenu()
     {
         MessageManager.Instance.ShowMessage("The villagers destroyed your castle!", 3f);
         yield return new WaitForSeconds(3f);
         SceneManager.LoadScene("MainMenu");
-    }
-
-    public void ShowTooltip(bool active)
-    {
-        tooltip.SetActive(active);
     }
 
     public void Upgrade()
@@ -87,7 +52,6 @@ public class Castle : MonoBehaviour
             health.MaxHealth = maxHealths[level];
             health.ChangeHealth(health.MaxHealth - health.currentHealth, gameObject);
             level++;
-            ShowTooltip(false);
         }
     }
 }
---
+++ Assets/Scripts/Utilities/TooltipObject.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TooltipObject : MonoBehaviour
+{
+    public Camera cam;
+    public GraphicRaycaster graphicRaycaster;
+
+    public LayerMask objectLayer;
+    public GameObject tooltip;
+    
+    private void OnEnable()
+    {
+        InputManager.Instance.OnLeftClickEvent += PerformLeftClick;
+        ShowTooltip(false);
+
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.OnLeftClickEvent -= PerformLeftClick;
+    }
+
+    private void PerformLeftClick(ClickEventArgs args)
+    {
+        Ray ray = cam.ScreenPointToRay(args.mousePosition);
+        RaycastHit hit;
+
+        PointerEventData data = new PointerEventData(EventSystem.current);
+        data.position = args.mousePosition;
+        List<RaycastResult> results = new List<RaycastResult>();
+        graphicRaycaster.Raycast(data, results);
+        
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, objectLayer, QueryTriggerInteraction.Ignore))
+        {
+            if (hit.transform.root == transform)
+            {
+                ShowTooltip(true);
+            }
+        }
+        else if (results.Count == 0)
+        {
+            ShowTooltip(false);
+        }
+    }
+    
+    public void ShowTooltip(bool active)
+    {
+        tooltip.SetActive(active);
+    }
+}
