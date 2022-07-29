086066ca84a7f34c1d1654935948de8be176a605
AaronMcDougall <72240339+AaronMcDougall@users.noreply.github.com>
2022-07-29 10:07:03 +1000

    Mute works again

--- Assets/Scripts/HUD UI/PauseSettingsUI.cs
+++ Assets/Scripts/HUD UI/PauseSettingsUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,9 +10,35 @@ public class PauseSettingsUI : MonoBehaviour
     public Toggle muteToggle;
 
     public float previousVolume;
+    
+    enum ScreenResolution
+    {
+        A,
+        B,
+        C
+    }
 
-    private float defaultVolume = 100;
+    private float defaultVolume = 1;
     private bool defaultMute = false;
+    
+    ScreenResolution screenResSelection(ScreenResolution resolution)
+    {
+        if (resolution == ScreenResolution.A)
+        {
+            Screen.SetResolution(1920, 1080, false);
+        }
+
+        if (resolution == ScreenResolution.B)
+        {
+            Screen.SetResolution(1280, 720, false);
+        }
+
+        if (resolution == ScreenResolution.C)
+        {
+            Screen.SetResolution(1280, 1040, false);
+        };
+        return resolution;
+    }
 
     private void FixedUpdate()
     {
@@ -37,7 +64,6 @@ public class PauseSettingsUI : MonoBehaviour
         if (!value)
         {
             volumeSlider.value = previousVolume;
-            muteToggle.isOn = false;
         }
     }
 
