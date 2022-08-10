d90187523f3a5e8e0dc30713a88b0840c139a28f
Tom Bartle <tombartle96@gmail.com>
2022-08-10 12:25:53 +1000

    Small random fixes

--- Assets/Scripts/AI/Soldier/SoldierFindTargetState.cs
+++ Assets/Scripts/AI/Soldier/SoldierFindTargetState.cs
@@ -30,7 +30,7 @@ public class SoldierFindTargetState : AntAIState
     {
         base.Enter();
         
-        castle = soldierModel.target;
+        castle = soldierModel.castle;
         pathfinding.FindPath(transform.position, castle.position);
         TargetCastle();
     }
